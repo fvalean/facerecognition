824c8b6f97fb076ff8e9d7c7af2b7e3ba4429d83
fvalean <36827144+fvalean@users.noreply.github.com>
2018-06-07 23:01:29 -0400

    dded placeholder text to immage url input box

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 			<p className='f3'>The brain will detect the faces in your image. Try it!</p>
 			<div className='center'>
 				<div className='center form pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+					<input className='f4 pa2 w-70 center' type='text' placeholder='Image URL' onChange={onInputChange}/>
 					<button 
 					className='w-30 grow f4 link ph3 pv2 dib white bg-light-red'
 					onClick={onButtonSubmit}
